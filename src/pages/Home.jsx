53884fa1fc89e3685189688dc0d829cf8bbd8532
siddharth kalani <siddharthkalani@siddharths-MacBook-Air.local>
2024-02-19 15:17:08 +0530

    feat:homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+   <div className="page">
+    <div className="card">
+        <div className='your-name text-2xl '>
+            <h1>Welcome Ramu,</h1>
+            </div>
+            <div className='expense text-xl'>
+             Current expense:<br></br>
+             ₹20,000
+            </div>
+            <div className="add-items">
+                <button className='items'>+last bill</button>
+                <button className='history'>history</button>
+            </div>
+            </div>
+    </div>
+        
+    
   )
 }
 
