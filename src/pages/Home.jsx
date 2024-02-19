2cadedb0c466c4194e24b1e51d26f1983fe45bc7
Toyash <toyashpatil17@gmail.com>
2024-02-19 22:04:31 +0530

    feat:card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,15 @@ import Cards from '../components/Cards'
 const Home = () => {
   return (
     <div className='flex items-start space-y-4 justify-center bg-slate-500 h-[100vh] w-[100vw] overflow-hidden '>
-      <div className='space-y-4'>
-        <div >
-          Name
+      <div className='space-y-[50px] p-2 w-[100%] '>
+        <div className=' mt-5 '>
+          <h1>Hello</h1>
         </div>
         <div className='w-[100vw] flex items-center justify-center '>
           <Cards />
+        </div>
+        <div className=''>
+          <h1>Hello</h1>
         </div>
       </div>
 
