496c428e76b427e1ad6000836164531fff6f22ba
siddkalani <siddharthkalani6@gmail.com>
2024-04-05 22:40:44 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -100,6 +100,30 @@ const Home = () => {
       </div>
      
 
+    // <div className='flex items-start space-y-4 justify-center bg-slate-500 h-[100vh] w-[100vw] overflow-hidden '>
+    //   <div className='space-y-[50px] w-[100%]' >
+    //     <div className='space-y-[50px] p-2 w-[100%] '>
+    //       <div className=' mt-5 '>
+    //         <h1>Hello</h1>
+    //       </div>
+    //       <div className='w-[100vw] flex items-center justify-center '>
+    //         <Cards />
+    //       </div>
+
+    //     </div>
+    //     <div className='bg-white flex rounded-tr-[30px] overflow-hidden rounded-tl-[30px] h-[100vh] items-start mt-10 p-2 '>
+    //       <div className=' p-2 overflow-hidden'>
+    //         <h1>Hello</h1>
+    //       </div>
+
+    //     </div>
+    //   </div>
+
+
+    // </div>
+
+    <div><Text/></div>
+    
   )
 }
 
