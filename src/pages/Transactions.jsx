d5ff7dac45e7b7527f348b85941cea7134a7e1fc
siddkalani <siddharthkalani6@gmail.com>
2024-04-06 15:04:06 +0530

    responsive

---
+++ src/pages/Transactions.jsx
@@ -0,0 +1,137 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from "../firebase"
+import { CiMoneyBill } from "react-icons/ci";
+import "./main.css"
+import { CgProfile } from "react-icons/cg";
+import { GrTransaction } from "react-icons/gr";
+import { FaHome } from "react-icons/fa";
+
+const Transactions = () => {
+    const [button1, setbutton1] = useState(false)
+    const [button2, setbutton2] = useState(false)
+    const [button3, setbutton3] = useState(false)
+    const [button4, setbutton4] = useState(false)
+    const [button5, setbutton5] = useState(false)
+    const [button6, setbutton6] = useState(false)
+    const [button7, setbutton7] = useState(false)
+    const [color, setColor] = useState("gray")
+  return (
+    <div>
+         <div className="container">
+      <div className="container-main">
+        <div className='header'>
+          <div className="name">
+            <div className='profile-name'>Transactions</div>
+          </div>
+          <div></div>
+        </div>
+        {/* <div className="gradient">
+
+          <div className="expense-card">
+            <div className="card-header">
+              <div> <CiMoneyBill size={34} /> </div>
+              <div className='Current-expense'>Current expense</div>
+            </div>
+            <div className="total-amount">₹1000</div>
+            <div className="card-btns">
+              <div className="btn1">
+                <div className='btn1-icon'></div>
+                <div className='btn1-content'>
+                  <div className='yesterday'>
+                    Yesterday
+                  </div>
+                  <div className='yesterday-amount'>
+                    ₹63
+                  </div>
+                </div>
+              </div>
+              <div className="btn2">
+                <div className='btn2-icon'></div>
+                <div className='btn2-content'>
+                  <div className='left'>
+                    Left
+                  </div>
+                  <div className='left-amount'>
+                    ₹1200
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="green"></div>
+          <div className="red"></div>
+        </div> */}
+        <div className='contain-body-transaction'>
+
+
+          {/* <div className="add-section">
+            <div className="add-header">Add tiffin:</div>
+            <div className="add-btns">
+              <div className="row1">
+                <button className='price glow-on-hover button1'>63</button>
+                <button className='price glow-on-hover button2'>83</button>
+              </div>
+              <div className="row2">
+                <button className='price glow-on-hover button3'>125</button>
+                <button className='price glow-on-hover button4'>0</button>
+              </div>
+            </div>
+            <div className="add-bill"><button className='glow-on-hover add-bill-btn button5'>Add bill</button></div>
+          </div> */}
+
+          <div className="trans-rows">
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+            <div className="input">your transaction</div>
+          </div>
+          <div className="footer">
+            <div className="footer-btns">
+              <div className={`home button6}`} onClick={() => {
+                setbutton1(false);
+                setbutton2(false);
+                setbutton3(false);
+                setbutton4(false);
+                setbutton5(false);
+                setbutton6(true);
+                setbutton7(false);
+              }}>
+                <FaHome size={40} color={button6 ? 'white' : 'gray'} />
+              </div>
+              <div className={`transactions button7`} onClick={() => {
+                setbutton1(false);
+                setbutton2(false);
+                setbutton3(false);
+                setbutton4(false);
+                setbutton5(false);
+                setbutton6(false);
+                setbutton7(true);
+              }}>
+                <GrTransaction size={40} color={button7 ? 'white' : 'gray'} />
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Transactions
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
+import Transactions from "../pages/Transactions";
 
 const routes = [
     {
@@ -11,6 +12,10 @@ const routes = [
         path: "/login",
         element: <Login />,
     },
+    {
+        path: "/transactions",
+        element: <Transactions />,
+    }
     ];
 
 export default routes;
